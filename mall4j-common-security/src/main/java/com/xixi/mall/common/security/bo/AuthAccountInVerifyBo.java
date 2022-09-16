544418e2267ad4abf2a93d3d573355539ff7d4e2
GootBird <xixichen698@outlook.com>
2022-09-16 21:14:54 +0800

    commit core

--- mall4j-common-security/src/main/java/com/xixi/mall/common/security/bo/AuthAccountInVerifyBo.java
+++ mall4j-common-security/src/main/java/com/xixi/mall/common/security/bo/AuthAccountInVerifyBo.java
@@ -1,18 +1,18 @@
 package com.xixi.mall.common.security.bo;
 
-//import com.xixi.mall.api.auth.bo.UserInfoInTokenBo;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-///**
-// * 用于校验的用户信息
-// */
-//@Getter
-//@Setter
-//public class AuthAccountInVerifyBo extends UserInfoInTokenBo {
-//
-//	private String password;
-//
-//	private Integer status;
-//
-//}
+import com.xixi.mall.api.auth.bo.UserInfoInTokenBo;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 用于校验的用户信息
+ */
+@Getter
+@Setter
+public class AuthAccountInVerifyBo extends UserInfoInTokenBo {
+
+	private String password;
+
+	private Integer status;
+
+}
